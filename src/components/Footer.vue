235c859ab82f29f7fa29cd59b9716a212c12bffe
Wency Baterna <wncbtrnfl@gmail.com>
2025-06-09 19:05:43 +0800

    added details in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,29 @@
 <template>
     <div class="h-24 bg-custom-primary flex flex-col items-center justify-center">
         <h1 class="text-white text-sm">Copyright &copy;2024 All Right Reserved </h1>
+        <div class="flex items-center space-x-6 mt-2">
+            <!-- Facebook Icon and Name -->
+            <a href="https://www.facebook.com/BestFortuneHotel?rdid=187aDSx0SpzGfVP6&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F1F6iC5dngh#" target="_blank" rel="noopener" aria-label="Facebook" class="flex items-center space-x-1 group">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" class="w-5 h-5 group-hover:fill-blue-500 transition-colors">
+                    <path d="M22.675 0h-21.35C.595 0 0 .592 0 1.326v21.348C0 23.408.595 24 1.325 24H12.82v-9.294H9.692v-3.622h3.127V8.413c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.797.143v3.24l-1.918.001c-1.504 0-1.797.715-1.797 1.763v2.313h3.587l-.467 3.622h-3.12V24h6.116C23.406 24 24 23.408 24 22.674V1.326C24 .592 23.406 0 22.675 0"/>
+                </svg>
+                <span class="text-white text-sm group-hover:underline">Best Fortune Hotel</span>
+            </a>
+            <!-- Email Icon and Address -->
+            <a href="mailto:bestfortunehotel@yahoo.com" class="flex items-center space-x-1 text-white text-sm hover:underline" aria-label="Email">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" class="w-5 h-5 mr-1">
+                    <path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 2v.01L12 13 4 6.01V6h16zM4 20v-9.99l7.99 7.99c.39.39 1.02.39 1.41 0L20 10.01V20H4z"/>
+                </svg>
+                <span>bestfortunehotel@yahoo.com</span>
+            </a>
+            <!-- Phone Icon and Number -->
+            <a href="tel:0915 595 9227" class="flex items-center space-x-1 text-white text-sm hover:underline" aria-label="Phone">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" class="w-5 h-5 mr-1">
+                    <path d="M6.62 10.79a15.053 15.053 0 006.59 6.59l2.2-2.2a1 1 0 011.01-.24c1.12.37 2.33.57 3.58.57a1 1 0 011 1V20a1 1 0 01-1 1C10.07 21 3 13.93 3 5a1 1 0 011-1h3.5a1 1 0 011 1c0 1.25.2 2.46.57 3.58a1 1 0 01-.24 1.01l-2.2 2.2z"/>
+                </svg>
+                <span>0915 595 9227</span>
+            </a>
+        </div>
     </div>
 </template>
 
