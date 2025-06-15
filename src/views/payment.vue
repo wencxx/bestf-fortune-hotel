02180cc10849e788d53bf3ca562aef003f184e38
Wency Baterna <wncbtrnfl@gmail.com>
2025-06-15 20:07:48 +0800

    added:number in payment

--- src/views/payment.vue
+++ src/views/payment.vue
@@ -11,6 +11,11 @@
         alt="GCash QR Code"
         class="w-64 h-64 object-contain border mb-6 rounded"
       />
+      <!-- Add GCash number below QR code -->
+      <div class="mb-6 text-center text-gray-700">
+        Can't scan? Send payment to GCash number:<br />
+        <span class="font-semibold text-lg">09306071273</span>
+      </div>
       <form @submit.prevent="submitPayment" class="w-full flex flex-col gap-4">
         <label class="font-medium">Reference Number</label>
         <input
